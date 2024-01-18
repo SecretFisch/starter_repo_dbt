fc7155e862bbea30920e9679f5dd6a39d095fbb5
SecretFisch <rettichretter@hotmail.de>
2024-01-18 21:12:15 +1100

    removed year inselect too

--- models/mart/weekly_avg_temp.sql
+++ models/mart/weekly_avg_temp.sql
@@ -1,6 +1,6 @@
 with total_avg AS(
     SELECT
-    city, country, year, lat, lon,
+    city, country, lat, lon, --year,
     AVG(avgtemp_c) AS avg_temp_week,
     MAX(maxtemp_c) AS max_temp_week,
     MIN(mintemp_c) AS min_temp_weekday
