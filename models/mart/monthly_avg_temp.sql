303203b0817b8495efe5dbfa1daff80a5429cda8
SecretFisch <rettichretter@hotmail.de>
2024-01-18 21:35:39 +1100

    table for months

---
+++ models/mart/monthly_avg_temp.sql
@@ -0,0 +1,10 @@
+with total_avg AS(
+    SELECT
+    city, country, lat, lon, month, weekday,
+    AVG(avgtemp_c) AS avg_temp_week,
+    MAX(maxtemp_c) AS max_temp_week,
+    MIN(mintemp_c) AS min_temp_weekday
+    FROM{{ref("prep_temp")}}
+    GROUP BY city, country, month, lat, lon, weekday
+)
+SELECT * FROM total_avg
