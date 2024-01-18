549ef958b5c37f1d143474cd51f67424138bbb15
SecretFisch <rettichretter@hotmail.de>
2024-01-18 21:23:38 +1100

    update for year again

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -7,7 +7,8 @@ add_weekday AS (
         to_char(date,'day') AS weekday,
         to_char(date,'DD') AS day_num,
         to_char(date,'week') AS week,
-        to_char(date,'month') AS month
+        to_char(date,'month') AS month,
+        to_char(date,'year') AS year
     FROM temp_daily
 )
 SELECT *
